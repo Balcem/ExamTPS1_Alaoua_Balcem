5d242b67d6c40588d5a191bbe579029d213ee708
balcem <alaouabalsem@gmail.com2>
2025-03-11 12:52:57 +0100

    Résolution de conflit et fusion de projets1 dans main

--- ExamTPS1/projet.py
+++ ExamTPS1/projet.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 def somme(T):
     S = 0
     for t in T:
@@ -10,4 +11,14 @@ Som=sum(Data)
 Data=[1,3,5]
 Som=sum(Data)
 print(La somme est :, Som)
+>>>>>>> projets1
+=======
+
+Data=[1,3,5]
+if Data:
+    print('La somme est :', sum(Data))
+    print('Le min est :', min(Data))
+    print('Le max est :', max(Data))
+else:
+    print('Dossier vide') 
 >>>>>>> projets1
